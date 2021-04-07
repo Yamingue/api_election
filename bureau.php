587a85f87b4a9ccbbc7d6bf0f19fc7a0b52fc39a
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-04-07 13:58:04 +0200

    api bureaux

--- bureau.php
+++ bureau.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-type: application/json');
 $respose['code'] = '200';
 if (isset($_POST['nom']) && !empty($_POST['nom'])) {
     # code...
@@ -7,6 +8,21 @@ if (isset($_POST['nom']) && !empty($_POST['nom'])) {
 else if (isset($_GET['nom']) && !empty($_GET['nom'])) {
     $electeur = $_GET['nom'];
 }else {
-    $respose['code'] = '500';
-    $respose['message'] = 'le contenue est vide';
+    $response['code'] = '500';
+    $response['message'] = 'le contenue est vide';
+    echo json_encode($response);
+    exit;
 }
+include_once 'config.php';
+$sql="SELECT nom.bureau as nom, quartier.bureau as quartier from burau,electeur WHERE nom.electeur='$electeur' AND id_bureau.electeur = id.bureau";
+$state = $database->query($sql);
+if ($state) {
+    $resulte = $state->fetchAll();
+    $response['datas'] = $resulte;
+} else {
+    # code...
+    $response['code'] ='500';
+    $response['message'] = 'Erruer technique';
+}
+
+echo json_encode($response);
